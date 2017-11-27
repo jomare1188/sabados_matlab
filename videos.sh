e315d51504a9079f8fbffe3aa626ee116a3ab4f6
jomare1188 <jomare1188@gmail.com>
2017-11-27 02:48:08 -0500

    script para concatenar videos

---
+++ videos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+touch aaa2123_ftyp.mov  
+touch aaa2123.mov
+
+touch bbb666_ftyp.mov  
+touch bbb666.mov 
+
+touch erdsfb1234_ftyp.mov
+touch erdsfb1234.mov 
+
+ls | sort -u | grep ftyp > lista_2.txt # lista ftpy
+ls | sort -u | grep -v ftyp | grep ".mov$" > lista_1.txt # lista .mov
+
+
+mapfile -t lista_ftyp < /home/jorge/scripts/lista_2.txt
+mapfile -t lista_mov < /home/jorge/scripts/lista_1.txt
+
+echo la longitud de array es ${#lista_ftyp[@]} # o cantidad de videos finales
+
+i=0
+
+while [ $i -lt ${#lista_ftyp[@]}  ] 
+do
+echo  ciclo "$i"
+cat ${lista_ftyp[i]} ${lista_mov[i]} > ftyp_mov_$i.mov; cat ${lista_mov[i]} ${lista_ftyp[i]} > mov_ftyp_$i.mov
+let i=$i+1
+done
